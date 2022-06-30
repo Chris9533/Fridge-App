fafa6a618f6516e7b9fba2189536f8c161f28863
Jack Kelsey <Jack@Admins-iMac.lan>
2022-06-30 14:29:20 +0100

    Stylesheet

--- Fridge-React/navigation/screens/HomeScreen.js
+++ Fridge-React/navigation/screens/HomeScreen.js
@@ -3,39 +3,9 @@ import { StyleSheet, View, Text, ScrollView, TouchableOpacity} from 'react-nativ
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards'
 import SearchBar from "react-native-dynamic-search-bar";
 import DropDownPicker from 'react-native-dropdown-picker';
+import { styles } from '../../stylesheet';
 
-const styles = StyleSheet.create({
-  card: {
-    borderRadius: '25%',
-    marginBottom: '3%'
-  },
-  container: {
-    display: 'grid',
-  },
-  red: {
-    color: 'red',
-  },
-  yellow: {
-    color: 'yellow',
-  },
-  green: {
-    color: 'green',
-  },
-  img: {
-    flex: 1,
-    width: 80,
-    height: 80,
-    resizeMode: 'contain' 
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  search: {
-    marginTop: '3%',
-    marginBottom: '3%'
-  }
-});
+
 
 export default function HomeScreen({navigation}) {
 
